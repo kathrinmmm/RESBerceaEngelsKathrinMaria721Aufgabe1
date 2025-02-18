c6eed64995c826fb678ba08c281cdf4efd87b593
kathrin <kathrin.bercea@stud.ubbcluj.ro>
2025-02-18 12:12:53 +0200

    a

--- src/main.java
+++ src/main.java
@@ -1,6 +1,53 @@
 package src;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
     }
 }
+import model.Haus;
+import parser.*;
+        import model.Result;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        String tsvFilePath="fisier.tsv";
+       TSVParser tsvParser = TSVParser.getInstance();
+
+        List<Result> punkteListe = new ArrayList<>();
+
+
+        try {
+
+//               punkteListe=tsvParser.parseResults(tsvFilePath);
+        } catch (IOException e) {
+            System.out.println("A apărut o eroare la citirea fișierului  " + e.getMessage());
+        }
+
+//a) lesen von der Datei und auf dem Bildschirm zeigen
+
+        for (Result result : punkteListe) {
+            System.out.println(result.toString());
+        }
+    }
+}
+
